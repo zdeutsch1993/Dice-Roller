99a069f8fbe901a189cfc987e2955a5a0c66e99f
zdeutsch1993 <zdeutsch1993@gmail.com>
2020-05-18 08:23:26 -0400

    Initial Commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+let dieRoll = [];
+let roll = document.querySelector('#roll');
+let die = document.querySelector('#die');
+let total = document.querySelector('#total');
+let totalRolls = document.querySelector('#totalRolls');
+roll.addEventListener("click", function() {
+    let dieValue = die.value;
+    let math = Math.floor(Math.random() * 6) + 1;
+})
+
+totalRolls.addEventListener("click", function(){
+
+})
