30f15df0c58e355f6ef5d37e882a997ee4fd609d
zdeutsch1993 <zdeutsch1993@gmail.com>
2020-05-18 23:14:50 -0400

    third commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,10 @@
 let dieRoll = [];
 let roll = document.querySelector('#roll');
 let die = document.querySelector('#die');
+let list = document.getElementById('#dynamicList');
+var li = document.createElement("LI");
 let total = document.querySelector('#total');
 let totalRolls = document.querySelector('#totalRolls');
-let listItem = document.createElement("li");
 roll.addEventListener("click", function() {
     let dieValue = die.value;
     for (let i = 0; i < dieValue; i++){
@@ -17,14 +18,13 @@ roll.addEventListener("click", function() {
 })
 
 totalRolls.addEventListener("click", function(){
-    let diceValue = die.value
-    for (let i = 0; i < diceValue; i++){
-        console.log(diceValue)  
-        let maths = Math.floor(Math.random() * 6) + 1; 
-        let str = '<ol>'
-        dieRoll.push(maths);
-        listItem.innerHTML = dieRoll.forEach(function(a,b){
-            str += '<li>' + a,b + '<li>';
-        })
+    let dieValue = die.value;
+    for (let i = 0; i < dieValue; i++){
+      console.log(dieValue)  
+      let math = Math.floor(Math.random() * 6) + 1; 
+      dieRoll.push(math);
+      li.innerHTML = dieRoll.reduce(function(a,b){
+          return a,b
+      }, 0)
     }
 })
